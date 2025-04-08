b5f1d43c6cfe77e225768cd4c5ea1b0cadcbc29d
koitialex <koitialex@gmail.com>
2025-04-08 17:14:52 -0300

    feat:valores inseridos para o banco

---
+++ formularioSimples.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE formulariosimples;
+ 
+CREATE TABLE cadastro(
+numero_cadastro INT NOT NULL,
+nome_completo VARCHAR(100) NOT NULL,
+data_nascimento DATE NOT NULL,
+cidade VARCHAR(50) NOT NULL,
+genero VARCHAR(20) NOT NULL
+);
